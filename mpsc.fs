efb46006c4ef7e08286294662fb73307ca3d79b0
Keith Pinson <kazark@zoho.com>
2017-10-12 16:45:16 -0400

    Throw some kinks in to the test

--- mpsc.fs
+++ mpsc.fs
@@ -1,27 +1,48 @@
 open System
+open System.Threading
 open System.Collections.Concurrent
 
+let mutable finished = false
 let blockingQueue = new BlockingCollection<string>(ConcurrentQueue<string>())
 
-let consume() =
-    while true do
-        let msg = blockingQueue.Take()
-        Console.ForegroundColor <- ConsoleColor.Cyan
-        eprintfn "and %s: ..." msg
+let consume cancel =
+    try
+        while not finished do
+            let msg = blockingQueue.Take(cancel)
+            Console.ForegroundColor <- ConsoleColor.Cyan
+            eprintfn "and %s: ..." msg
+            Console.ForegroundColor <- ConsoleColor.White
+            Thread.Sleep(msg.Length * 10)
+    with _ ->
+        Console.ForegroundColor <- ConsoleColor.DarkCyan
+        eprintfn "That's all."
         Console.ForegroundColor <- ConsoleColor.White
-    Console.ForegroundColor <- ConsoleColor.DarkCyan
-    eprintfn "That's all."
-    Console.ForegroundColor <- ConsoleColor.White
+
+let readLine() =
+    try
+        Console.ReadLine()
+    with ex ->
+        printfn "ReadLine exception: %O" ex
+        null
 
 [<EntryPoint>]
 let main _ =
-    async {
-        consume()
-    } |> Async.Start
-    while true do
-        Console.ForegroundColor <- ConsoleColor.DarkMagenta
-        printf "What else can you tell me? "
-        Console.ForegroundColor <- ConsoleColor.White
-        Console.ReadLine().Split ' '
-        |> Array.iter blockingQueue.Add
+    Async.Start(async { consume(Async.DefaultCancellationToken) }, Async.DefaultCancellationToken)
+    try
+        let rand = Random()
+        Seq.initInfinite (fun _ ->
+            Thread.Sleep(rand.Next(1,100))
+            Console.ForegroundColor <- ConsoleColor.DarkMagenta
+            printf "What else can you tell me? "
+            Console.ForegroundColor <- ConsoleColor.White
+            readLine()
+        ) |> Seq.takeWhile ((<>) null)
+        |> Seq.iter (fun (x : string) ->
+            x.Split ' '
+            |> Array.filter (not << String.IsNullOrWhiteSpace)
+            |> Array.iter blockingQueue.Add
+        )
+    with ex ->
+        printfn "Exception: %O" ex
+    Async.CancelDefaultToken()
     0
